12d82817b39cc45c8d3c2c411b0d3f4b1085bd05
teohan-eksi <teohan.eksi@gmail.com>
2021-05-01 07:09:47 +0300

    added new file via git

---
+++ runcpp.sh
@@ -0,0 +1,19 @@
+
+
+
+file_name="file name"
+make $file_name
+
+./$file_name
+
+#---- For multiple files
+
+#g++ -c FileName1.cpp
+#g++ -c FileName2.cpp
+
+#g++ -o runnable FileName1.o FileName2.o
+
+#./runnable
+
+echo "\nPress enter to quit."
+read var
